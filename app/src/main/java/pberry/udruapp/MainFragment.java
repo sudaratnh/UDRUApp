ebed1026e2e201b2186d4eac0e51904e81e011f8
Sudarat <sudarat.n@udru.ac.th>
2018-11-19 15:33:07 +0700

    Replace Fragment Success

--- app/src/main/java/pberry/udruapp/MainFragment.java
+++ app/src/main/java/pberry/udruapp/MainFragment.java
@@ -2,10 +2,12 @@ package pberry.udruapp;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,10 +15,34 @@ import android.view.ViewGroup;
  */
 public class MainFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//      Register Controller (Something can click)
+
+        TextView textView = getView().findViewById(R.id.textViewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragement
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.LayoutMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+
+
+    } // Main Method
 
     public MainFragment() {
         // Required empty public constructor
-    }
+
+    } // Constructor
 
 
     @Override
@@ -24,6 +50,6 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+    } //Create View
 
-}
+}//Main Class
