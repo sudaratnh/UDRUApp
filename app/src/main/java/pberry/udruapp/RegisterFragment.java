beea8d9ed0eecf2c778ea9e2c58464f9537fdc03
sudarat <sudarat.nh@gmail.com>
2018-11-21 09:19:16 +0700

    Connect firebase

--- app/src/main/java/pberry/udruapp/RegisterFragment.java
+++ app/src/main/java/pberry/udruapp/RegisterFragment.java
@@ -1,14 +1,17 @@
 package pberry.udruapp;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -19,48 +22,53 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
 
-    //    EXplicit
+    //    Explicit
     private Uri uri;
-    private  ImageView imageView;
+    private ImageView imageView;
     private boolean aBoolean = true;
-
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
 //        Create Toolbar
-
         createToolbar();
 
 //        Avata Controller
-
         avataController();
 
-    } // Main Method
+
+    }   // Main Method
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
         if (item.getItemId() == R.id.itemUpload) {
 
-            checkData();
+            cheackData();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    private void checkData() {
+    private void cheackData() {
 
         MyAlert myAlert = new MyAlert(getActivity());
-    //Get Value From EditText to string
+
+//        Get Value From EditText to String
         EditText nameEditText = getView().findViewById(R.id.edtName);
         EditText emailEditText = getView().findViewById(R.id.edtEmail);
         EditText passwordEditText = getView().findViewById(R.id.edtPassword);
@@ -71,29 +79,93 @@ public class RegisterFragment extends Fragment {
         String passwordString = passwordEditText.getText().toString().trim();
         String rePasswordString = rePasswordEditText.getText().toString().trim();
 
-
         if (aBoolean) {
             myAlert.normalDialog("No Avata", "Please Choose Image for Avata");
         } else if (checkSpace(nameString, emailString, passwordString, rePasswordString)) {
             myAlert.normalDialog(getString(R.string.title_have_space), getString(R.string.message_have_space));
-
         } else if (passwordString.equals(rePasswordString)) {
-
-            uploadTofirebase(nameString, emailString, passwordString);
+//            Password Math
+            uploadToFirebase(nameString, emailString, passwordString);
         } else {
-            myAlert.normalDialog("Password Not Match","Please type Password again");
+            myAlert.normalDialog("Password Not Math", "Please Type Password agains");
         }
+
     }
 
-    private void uploadTofirebase(String nameString, String emailString, String passwordString) {
 
+    private void uploadToFirebase(final String nameString, String emailString, String passwordString) {
+
+        final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Please Wait...");
+        progressDialog.show();
+
+//        upload Image
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageReference = firebaseStorage.getReference();
+        StorageReference storageReference1 = storageReference.child("Avata/" + nameString);
+        storageReference1.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Toast.makeText(getActivity(), "Success Upload", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+
+                //        Register Email
+                String urlAvata = findURlavata(nameString);
+                Log.d("20novV1", "urlAvata ==> " + urlAvata);
+
+
+
+
+
+            }   // onSuccess
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Cannot Upload", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+            }
+        });
+
+
+
+
+
+
+    }   // upload
+
+    private String findURlavata(String nameString) {
+
+
+        return myFindURL(nameString);
+    }
+
+    private String myFindURL(String nameString) {
+        FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageReference = firebaseStorage.getReference();
+
+        final String[] strings = new String[1];
+        storageReference.child("Avata").child(nameString)
+                .getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        strings[0] = uri.toString();
+                        return;
+
+                    }
+                });
+        return strings[0];
     }
 
     private boolean checkSpace(String nameString,
                                String emailString,
                                String passwordString,
                                String rePasswordString) {
+
         boolean result = false;
+
         if (nameString.isEmpty() || emailString.isEmpty() || passwordString.isEmpty() || rePasswordString.isEmpty()) {
             result = true;
         }
@@ -130,28 +202,31 @@ public class RegisterFragment extends Fragment {
 
 
         } else {
-            Toast.makeText(getActivity(),"Please Choose Image", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(),
+                    "Please Choose Image",
+                    Toast.LENGTH_SHORT).show();
         }
 
-
-    } //Result
+    }   // Result
 
     private void avataController() {
         imageView = getView().findViewById(R.id.imageViewAvata);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(intent, "Please Choose App and Image"),5);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Please Choose App and Image"), 5);
+
             }
         });
     }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
-        //Do trash
-        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(R.string.message_have_space);
         ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
@@ -160,12 +235,10 @@ public class RegisterFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 getActivity().getSupportFragmentManager().popBackStack();
-
             }
         });
 
         setHasOptionsMenu(true);
-
 
     }
 
